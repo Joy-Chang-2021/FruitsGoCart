ee8c862f28f59e43ea73237c529c6e87957f9a8c
Joy-Chang-2021 <joy50422@gmail.com>
2022-12-01 04:16:10 +0800

    add: 框架 header、主題色彩、主題按鈕樣式、黑暗模式函式

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,38 @@ import './scss/App.scss'
 </script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Index</RouterLink>
-    <RouterLink to="/Cart">Cart</RouterLink>
-  </nav>
-  <RouterView />
+  <div class="w-80 min-w-full mx-auto">
+    <header>
+      <div class="max-w-screen-lg mx-auto p-3 flex justify-between">
+        <!-- LOGO -->
+        <h1>
+          Fruits 水果購
+        </h1>
+        <!--darkMode/登入/註冊 -->
+        <div class="flex items-center">
+          <img src="/src/assets/darkMode.png" alt="模式" @click="darkMode" class="w-6 cursor-pointer">
+          <button class="btn btn-outline ml-3">登入</button>
+          <button class="btn btn-theme ml-2">註冊</button>
+        </div>
+      </div>
+    </header>
+    <main class="max-w-screen-lg mx-auto">
+      <nav>
+        <RouterLink to="/">Index</RouterLink>
+        <RouterLink to="/Cart">Cart</RouterLink>
+      </nav>
+      <RouterView />
+    </main>
+  </div>
+  
 </template>
+
+<script>
+export default {
+  methods: {
+    darkMode() {
+      document.body.classList.toggle("dark-mode")
+    } 
+  }
+}
+</script>

00fa41fc587f0a6ff56581add290572dd649f03f
Joy-Chang-2021 <joy50422@gmail.com>
2022-12-06 03:28:18 +0800

    feat: OrderShipping 輸入資料綁定 v-model

--- src/components/OrderShipping.vue
+++ src/components/OrderShipping.vue
@@ -5,8 +5,8 @@
     <div class="ordering flex flex-col sm:flex-row">
       <p class="orderName sm:w-1/4 text-left sm:text-right px-2">小計</p>
       <p class="sm:my-1 sm:w-3/4 flex flex-row justify-between px-2">
-        <span>共計 2 項</span>
-        <span>$ 1260</span>
+        <span>共計 {{ "2" }} 項</span>
+        <span>$ {{ productsCost }}</span>
       </p>
     </div>
     <!-- 取貨方式 -->
@@ -53,7 +53,7 @@
         <span>
           <button class="btn btn-theme py-1 px-2">全站運費折抵卷</button>
         </span>
-        <span>${{ 1260 + shippingFee }}</span>
+        <span>${{ productsCost + shippingFee }}</span>
       </p>
     </div>
     <!-- 同意條款 -->
@@ -61,7 +61,9 @@
       <p class="orderName sm:w-1/4 text-left sm:text-right px-2">同意條款</p>
       <p class="sm:my-1 sm:w-3/4 flex flex-row justify-between px-2">
         <span>
-          <input type="checkbox" id="termsOfService">
+          <input type="checkbox" id="termsOfService" 
+            v-model="termsOfService" 
+            :true-value="true" :false-value="false">
           <label for="termsOfService">
             我已詳細閱讀並且同意水果購
           </label>
@@ -78,11 +80,11 @@
       </span>
     </h4>
     <p class="orderDone">
-      <span>店取：7-11</span>
-      <span class="mx-24">取貨付款</span>
+      <span>{{ shippingWay }}</span>
+      <span class="mx-24">{{ payingWay }}</span>
       <span>
-        <input type="radio" id="checkedMoney" v-model="checkedMoney" value="1290元">
-        <label for="checkedMoney" class="ml-1">1290元</label>
+        <input type="radio" id="checkedTotal" v-model="checkedTotal" value="checked">
+        <label for="checkedTotal" class="ml-1">{{ productsCost + shippingFee }}</label>
       </span>
     </p>
   </div>
@@ -90,19 +92,19 @@
   <div :class="{'hidden': checkoutState !== 3}">
     <h4 class="orderDone">
       <span class="orderCategory">
-        付款明細 - 超商取貨付款
+        付款明細 - {{ shippingConclusion }}
       </span>
     </h4>
     <div class="ordering flex flex-col sm:flex-row">
       <p class="orderName sm:w-1/4 text-left sm:text-right px-2">訂單編號</p>
       <p class="sm:my-1 sm:w-3/4 flex flex-row justify-between px-2">
-        <span>CM2212043524697</span>
+        <span>{{ "CM2212043524697" }}</span>
       </p>
     </div>
     <div class="ordering flex flex-col sm:flex-row">
       <p class="orderName sm:w-1/4 text-left sm:text-right px-2">訂單時間</p>
       <p class="sm:my-1 sm:w-3/4 flex flex-row justify-between px-2">
-        <span>2022/12/04 09:53</span>
+        <span>{{ "2022/12/04 09:53" }}</span>
       </p>
     </div>
     <div class="ordering flex flex-col sm:flex-row">
@@ -132,7 +134,9 @@ export default {
     return {
       shippingWay: "店取：Fruits Go",
       payingWay: "取貨付款",
-      checkedMoney: "",
+      productsCost: 2000, //商品總額 //TODO: 數字暫定,待資料串接
+      checkedTotal: "", //input[type="radio"]確認總金額
+      termsOfService: false, //input[type="checkbox"]詳閱同意服務條款
     }
   },
   computed: {
@@ -140,6 +144,16 @@ export default {
       // 依照運送方式、顯示運費
       if (this.shippingWay === "店取：Fruits Go") return 35
       else return 99
+    },
+    shippingConclusion() {
+      // state(3) 付款明細 - 標題文字
+      let conclusion = ""
+      if (this.shippingWay === "店取：Fruits Go") conclusion += "Fruits Go 商店取貨"
+      else if (this.shippingWay === "店到宅：宅配") conclusion += "到宅配送"
+      if (this.payingWay === "取貨付款") conclusion += "/取貨付款"
+      else if (this.payingWay === "銀行轉帳") conclusion += "/銀行轉帳"
+      else if (this.payingWay === "信用卡") conclusion += "/信用卡付款"
+      return conclusion
     }
   }
 }

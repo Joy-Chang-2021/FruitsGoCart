29220e60134d58ec394dbcac25d0d57c94c94b06
Joy-Chang-2021 <joy50422@gmail.com>
2022-12-09 01:53:00 +0800

    chore: 測試 mock service worker 直接佈署後的串接結果

--- src/main.js
+++ src/main.js
@@ -6,14 +6,15 @@ import './scss/App.scss'
 
 const app = createApp(App)
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start({
-    // Specifies how to handle a request that is not listed in the request handlers.
-    // ex: GET /src/assets/money.svg
-    // ex: GET /src/views/Cart.vue
-    onUnhandledRequest: 'bypass'
-  })
-}
+// if(在開發環境下)、使用 mock service worker 協助串接 mock API
+// if (process.env.NODE_ENV === 'development') {}
+worker.start({
+  // Specifies how to handle a request that is not listed in the request handlers.
+  // ex: GET /src/assets/money.svg
+  // ex: GET /src/views/Cart.vue
+  onUnhandledRequest: 'bypass'
+})
+
 
 app.use(router)
 

fdf29fb6200d48d7b3e8c4e6129d03ca781fc17f
Joy-Chang-2021 <joy50422@gmail.com>
2022-12-08 20:55:16 +0800

    feat: 套件 mock service worker

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { worker } from './mocks/browser'
 import './scss/App.scss'
 
 const app = createApp(App)
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 app.use(router)
 
